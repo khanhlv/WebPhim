7c1de420bdfff004c3d8d9ca9d59f6f8b1c0b1de
khanhlv5 <khanhlv5@vpbank.com.vn>
2019-04-18 14:25:20 +0700

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,119 @@
-$(document).ready(function(){
-
-    // Home
-    fetchDataSheet("1NXqQKdHCIgs19Vhy8M2Aq4tuj8CKNLABJXekGdOAiL0", "1", function(data) {
-        makeData(data, "home");
-    });
-
-    // Top
-    fetchDataSheet("1NXqQKdHCIgs19Vhy8M2Aq4tuj8CKNLABJXekGdOAiL0", "2", function(data) {
-        makeData(data, "top");
-    });
+'use strict';
+function fetchDataSheet(sheetId, sheetIndex, callback) {
+    var sheetUrl = 'https://spreadsheets.google.com/feeds/cells/' + sheetId + '/' + sheetIndex + '/public/full?alt=json';
     
-    function fetchDataSheet(sheetId, sheetIndex, callback) {
-        var sheetUrl = 'https://spreadsheets.google.com/feeds/cells/' + sheetId + '/' + sheetIndex + '/public/full?alt=json';
-        
-        $.getJSON(sheetUrl, function(data){
-            var entry = data.feed.entry;
-            var data = [];
-            var columns = 6;
-            for (var i = columns, lengEntry = entry.length; i < lengEntry; i += columns) {
-                var rows = []; 
-                for (var j = 0; j < columns; j++) {
-                    rows.push(entry[i + j] ? entry[i + j].content.$t : '')
-                }
-                data.push(rows);
+    $.getJSON(sheetUrl, function(data){
+        var entry = data.feed.entry;
+        var data = [];
+        var columns = 7;
+        for (var i = columns, lengEntry = entry.length; i < lengEntry; i += columns) {
+            var rows = []; 
+            for (var j = 0; j < columns; j++) {
+                rows.push(entry[i + j] ? entry[i + j].content.$t : '')
             }
+            data.push(rows);
+        }
 
-            callback(data);
+        callback(data);
+    });
+}
+
+function makeData(data, key, items) {
+    items.empty();
+   
+    for (var i = 0, lengData = data.length; i < lengData; i++) {
+        var html = '';
+        
+        if (key == 'home') {
+            html += '<div class="col-md-3">';
+        } else {
+            html += '<div class="col-md-12">';
+        }
+
+        html +=    '        <div class="box">';
+        html +=    '           <a target="_bank" href="detail.html?meta=' + data[i][0] + '" alt="' + data[i][1] + '" title="' + data[i][1] + '">';
+        
+        if (key == 'home') {
+            html +=    '                <div class="box-img-left" style="background-image: url(' + data[i][2] + ');"></div>';
+        } else {
+            html +=    '                <div class="box-img-right" style="background-image: url(' + data[i][2] + ');"></div>';
+        }
+
+        if (key == 'home') {
+            html +=    '                <div class="text-title">' + data[i][1] + '</div>';
+        } else {
+            html +=    '                <div class="text-title mb-10">' + data[i][1] + '</div>';
+        }
+
+        if (data[i][5] != 'N/A')
+        {
+            html +=    '                <div class="text-summary">' + data[i][5] + '</div>';
+        }
+        
+        html +=    '            </a>';
+        html +=    '        </div>';
+        html +=    '    </div>';
+
+        items.append(html);
+    }
+}
+
+function getValueQuery(key) {
+    const urlParams = new URLSearchParams(window.location.search);
+    
+    return urlParams.get(key);
+}
+
+function detailContent() {
+    var meta = getValueQuery('meta');
+    console.log(meta);
+
+    fetchDataSheet("1NXqQKdHCIgs19Vhy8M2Aq4tuj8CKNLABJXekGdOAiL0", "1", function(data) {
+        var item = data.filter(function(d) {
+            return d[0] === meta;
+        })[0];
+
+        $('.content-detail').empty();
+        
+        if (item) {
+            var template = `
+                <h2 class="mb-15 text-center text-uppercase" style="color:#f9005a">${item[1] == 'N/A' ? '' : item[1]}</h3>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="text-center">
+                            <img src="${item[6] == 'N/A' ? item[2] : item[6]}">
+                            <div class="watching">
+                                <a target="_bank" href="${item[4] == 'N/A' ? '' : item[4]}" class="watching"><button type="button" class="btn btn-watching">Xem online</button></a>
+                                <a target="_bank" href="${item[4] == 'N/A' ? '' : item[4]}" class="download"><button type="button" class="btn btn-download">Tải về</button></a>
+                            </div>	
+                            
+                        </div>
+                        
+                        <div class="text-center mt-20">
+                            ${item[3] == 'N/A' ? '' : item[3]}
+                        </div>
+
+                    </div>
+                </div>
+            `;
+            
+            $('.content-detail').append(template);
+            $('title').html(item[1] == 'N/A' ? '' : item[1] + ' | PhimNetClub')
+        } else {
+            $('.content-detail').html("<h2 style='color: #8a2be2' class='text-center'>Không tìm thấy thông tin phim</h2>")
+        }
+    });
+}
+$(document).ready(function() {
+    if ($('.home-page').length > 0) {
+        fetchDataSheet("1NXqQKdHCIgs19Vhy8M2Aq4tuj8CKNLABJXekGdOAiL0", "1", function(data) {
+            makeData(data, "home", $('.list-items'));
         });
     }
 
-    function makeData(data,key) {
-        var items = $('.list-items');
-        if (key == 'home') {
-            $('.list-items').empty();
-            items = $('.list-items');
-        } else {
-            $('.list-items-top').empty();
-            items = $('.list-items-top');
-        }
-
-        for (var i = 0, lengData = data.length; i < lengData; i++) {
-            var html = '';
-            
-            if (key == 'home') {
-                html += '<div class="col-md-3">';
-            } else {
-                html += '<div class="col-md-12">';
-            }
-
-            html +=    '        <div class="box">';
-            html +=    '           <a target="_bank" href="' + data[i][4] + '" alt="' + data[i][1] + '" title="' + data[i][1] + '">';
-            
-            if (key == 'home') {
-                html +=    '                <div class="box-img-left" style="background-image: url(' + data[i][2] + ');"></div>';
-            } else {
-                html +=    '                <div class="box-img-right" style="background-image: url(' + data[i][2] + ');"></div>';
-            }
-
-            if (key == 'home') {
-                html +=    '                <div class="text-title">' + data[i][1] + '</div>';
-            } else {
-                html +=    '                <div class="text-title mb-10">' + data[i][1] + '</div>';
-            }
-
-            if (data[i][5] != 'N/A')
-            {
-                html +=    '                <div class="text-summary">' + data[i][5] + '</div>';
-            }
-            
-            html +=    '            </a>';
-            html +=    '        </div>';
-            html +=    '    </div>';
-
-            items.append(html);
-        }
+    if ($('.detail-page').length > 0) {
+        detailContent();
     }
-  });
+    
+});
+
---
+++ assets/js/webcomponent.js
@@ -0,0 +1,61 @@
+class HeaderLayout extends HTMLElement {
+    constructor() {
+      super();
+      const shadowRootEl = this.attachShadow({mode: 'open'});
+      shadowRootEl.innerHTML = `
+        <style>
+            @import "/assets/css/main.css";
+        </style>
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading"><a href="/"><span style="color:cadetblue">[PhimNetClub]</span></a></h1>
+                <p class="lead text-muted">LUÔN MANG NHỮNG TUYỆT PHẨM PHIM ẢNH ĐẾN VỚI BẠN SỚM NHẤT – CHẤT LƯỢNG NHẤT.</p>
+            </div>
+        </section>  
+      `;
+    }
+}
+window.customElements.define('header-layout', HeaderLayout);
+
+class FooterLayout extends HTMLElement {
+    constructor() {
+      super();
+      const shadowRootEl = this.attachShadow({mode: 'open'});
+      shadowRootEl.innerHTML = `
+        <style>
+            @import "/assets/css/main.css";
+        </style>
+        <footer class="text-muted">
+            <div class="container">
+                <p class="float-right"><a href="#">Lên đầu trang</a></p>
+                <p>&copy;PhimNetClub 2019, Cảm ơn bạn đã đồng hành. Chúc bạn luôn vui vẻ trong cuộc sống và nhiều giây phút thư giãn!</p>
+                <p>Liên hệ: <a href="mailto:phimnet.club@gmail.com">phimnet.club@gmail.com</a> | <a target="_bank" href="complaints.html">Khiếu nại bản quyền</a> | <a target="_bank" href="donate.html">Quyền góp (Donate)</a>.</p>
+            </div>
+        </footer> 
+      `;
+    }
+}
+window.customElements.define('footer-layout', FooterLayout);
+
+class TopFilmLayout extends HTMLElement {
+    constructor() {
+      super();
+      const shadowRootEl = this.attachShadow({mode: 'open'});
+      shadowRootEl.innerHTML = `
+        <style>
+            @import "/assets/css/main.css";
+        </style>
+        <h3 class="mb-15 group-title">Bảng xếp hạng</h3>
+        <div class="row list-items-top">
+            <div class="col-md-12">
+                <p class="loading">Đang lấy dữ liệu ....</p>
+            </div>
+        </div>
+      `;
+        fetchDataSheet("1NXqQKdHCIgs19Vhy8M2Aq4tuj8CKNLABJXekGdOAiL0", "2", function(data) {
+            makeData(data, "top", $(shadowRootEl).find('.list-items-top'));
+        });
+
+    }
+}
+window.customElements.define('top-film-layout', TopFilmLayout);
