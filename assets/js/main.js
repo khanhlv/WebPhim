ec40256f2b43c7cfd2a8722bdaaa488f070d31b3
khanhlv5 <khanhlv5@vpbank.com.vn>
2019-04-22 13:28:35 +0700

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,21 +58,18 @@ function makeData(data, key, items) {
     }
 }
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
 function getValueQuery(key) {
     const urlParams = new URLSearchParams(window.location.search);
 
     if (!urlParams) {
-        return getParameterByName(key)
+        return getUrlParameter(key)
     }
     
     return urlParams.get(key);
